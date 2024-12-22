a8afaab3080265a0b66aa61f644a807a83a79fac
Suicidal-Cat <darekolaric@gmail.com>
2024-12-22 13:33:21 +0100

    add basic move logic

--- client/src/client/core.cljs
+++ client/src/client/core.cljs
@@ -2,31 +2,40 @@
     (:require [reagent.dom :as rdom]
               [reagent.core :as r]
               [quil.core :as q]
+              [clojure.edn :as edn]
               ))
 
 (enable-console-print!)
 
 
 (defonce app-state (atom {:text "Hello world!"}))
+(def snake-body1 (r/atom [[180 100] [160 100] [140 100] [120 100] [100 100]]))
+(def snake-body2 (r/atom [[300 120] [300 140] [300 160] [300 180] [300 200]]))
 
 
 (defn on-js-reload []
 )
 
-
-;; (def socket
-;;   (ws/connect
-;;    "ws://localhost:8080/ws"
-;;    :on-receive #(prn 'received %)))
-;; ;(ws/send-msg socket "meow32121")
-;; ;(ws/close socket)
-
 (def ws (js/WebSocket. "ws://localhost:8080/ws"))
 
-(.addEventListener ws "open" (fn [_] (println "WebSocket connected")  (.send ws "Hello from ClojureScript!")))
-(.addEventListener ws "message" (fn [e] (println "Message received:" (.-data e))))
+(.addEventListener ws "open" (fn [_] (println "WebSocket connected")))
+(.addEventListener ws "message" (fn [e]
+                                  (let [data (edn/read-string (.-data e))]
+                                    ;(println data)
+                                    (reset! snake-body1 (:snake1 data))
+                                    (reset! snake-body2 (:snake2 data))
+                                    )))
 (.addEventListener ws "close" (fn [_] (println "WebSocket closed")))
 
+(defn handle-keypress [e]
+  (let [key (.-key e)]
+    (case key
+      "ArrowUp" (.send ws {:direction :up})
+      "ArrowDown" (.send ws {:direction :down})
+      "ArrowLeft" (.send ws {:direction :left})
+      "ArrowRight" (.send ws {:direction :right}))))
+
+(.addEventListener js/document "keydown" handle-keypress)
 
 (defn canvas []
   [:div {:id "game-canvas"}])
@@ -36,16 +45,13 @@
    [:p "Snake game"]
    [canvas]])
 
-(def snake-body1 (r/atom [[100 100] [120 100] [140 100] [160 100] [180 100]]))
-(def snake-body2 (r/atom [[300 120] [300 140] [300 160] [300 180] [300 200]]))
-
 (defn setup []
-  (q/frame-rate 1) 
+  (q/frame-rate 25) 
   (q/background 0))
 
 (defn draw []
   (q/background 0)
-  (q/stroke 0)     ;; Crna ivica
+  (q/stroke 0)
   (q/stroke-weight 2)
   (q/fill 0 255 0)
   (doseq [[x y] @snake-body1]
@@ -62,5 +68,5 @@
  :settings #(q/smooth 2)
  :setup setup
  :draw draw
- :size [800 600])
+ :size [700 500])
 
--- server/src/server/core.clj
+++ server/src/server/core.clj
@@ -1,22 +1,73 @@
 (ns server.core
   (:gen-class)
-  (:require [compojure.core :refer :all]
+  (:require [compojure.core :refer [defroutes GET]]
             [ring.adapter.jetty :refer [run-jetty]]
             [ring.websocket :as ws]
-            ;[ring.util.response :refer [file-response]]
+            [clojure.edn :as edn]
             ))
+
+
+(def game-state (atom {:snake1 [[100 100] [120 100] [140 100] [160 100] [180 100]]
+                       :snake2 [[300 120] [300 140] [300 160] [300 180] [300 200]]}))
+
+(def snake-direction (atom {:snake1 :right
+                      :snake2 :left}))
+
+(defn move-snake [snake direction]
+  (let [[x y] (first snake)]
+    (case direction
+      :up    (cons [x (- y 20)] (butlast snake))
+      :down  (cons [x (+ y 20)] (butlast snake))
+      :left  (cons [(- x 20) y] (butlast snake))
+      :right (cons [(+ x 20) y] (butlast snake)))))
+
+(defn change-direction [snake dir]
+  (let [past-dir (snake @snake-direction)]
+    (if (not= past-dir dir)
+      (cond
+        (and (= past-dir :up) (not= dir :down)) (swap! snake-direction update snake (fn [_] dir))
+        (and (= past-dir :down) (not= dir :up)) (swap! snake-direction update snake (fn [_] dir))
+        (and (= past-dir :left) (not= dir :right)) (swap! snake-direction update snake (fn [_] dir))
+        (and (= past-dir :right) (not= dir :left)) (swap! snake-direction update snake (fn [_] dir))))))
+
+(defn handle-message [message]
+  (let [data (edn/read-string message)
+        dir (:direction data)]
+    (println dir)
+    (change-direction :snake1 dir)
+    ))
+
+(def stop-flag (atom false))
+
+(defn broadcast-game-state [socket]
+  (future
+    (reset! stop-flag false)
+    (while (not @stop-flag)
+      (Thread/sleep 100)
+      (swap! game-state update :snake1 move-snake (:snake1 @snake-direction))
+      (ws/send socket (pr-str @game-state)))))
+
 
 (defn echo-handler [request]
   (assert (ws/upgrade-request? request))
   {::ws/listener
    {:on-open
     (fn [socket]
-      (ws/send socket "I will echo your messages"))
+      ;(ws/send socket "I will echo your messages")
+      (reset! game-state {:snake1 [[180 100] [160 100] [140 100] [120 100] [100 100]]
+                          :snake2 [[300 120] [300 140] [300 160] [300 180] [300 200]]})
+      (broadcast-game-state socket)
+      )
     :on-message
     (fn [socket message]
       (if (= message "exit")
-        (ws/close socket)
-        (ws/send socket message)))}})
+        (ws/close socket) 
+        (handle-message message)
+        ))
+    :on-close
+    (fn [socket code reason]
+      (reset! stop-flag true))
+    }})
 
 (defroutes app-routes
   (GET "/" [] "<h1>HELLO<h1>")
