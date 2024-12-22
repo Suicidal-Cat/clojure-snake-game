aa84be4b2b2c1eed32755b82ac094d1e2fb74c0f
Suicidal-Cat <darekolaric@gmail.com>
2024-12-22 19:03:43 +0100

    refactor client project

--- client/src/client/core.cljs
+++ client/src/client/core.cljs
@@ -1,41 +1,18 @@
 (ns client.core
-    (:require [reagent.dom :as rdom]
-              [reagent.core :as r]
-              [quil.core :as q]
-              [clojure.edn :as edn]
-              ))
+    (:require
+     [client.game :refer [connect_socket start_game]]
+     [reagent.dom :as rdom]))
 
 (enable-console-print!)
 
 
 (defonce app-state (atom {:text "Hello world!"}))
-(def snake-body1 (r/atom [[180 100] [160 100] [140 100] [120 100] [100 100]]))
-(def snake-body2 (r/atom [[300 120] [300 140] [300 160] [300 180] [300 200]]))
 
 
 (defn on-js-reload []
 )
 
-(def ws (js/WebSocket. "ws://localhost:8080/ws"))
 
-(.addEventListener ws "open" (fn [_] (println "WebSocket connected")))
-(.addEventListener ws "message" (fn [e]
-                                  (let [data (edn/read-string (.-data e))]
-                                    ;(println data)
-                                    (reset! snake-body1 (:snake1 data))
-                                    (reset! snake-body2 (:snake2 data))
-                                    )))
-(.addEventListener ws "close" (fn [_] (println "WebSocket closed")))
-
-(defn handle-keypress [e]
-  (let [key (.-key e)]
-    (case key
-      "ArrowUp" (.send ws {:direction :up})
-      "ArrowDown" (.send ws {:direction :down})
-      "ArrowLeft" (.send ws {:direction :left})
-      "ArrowRight" (.send ws {:direction :right}))))
-
-(.addEventListener js/document "keydown" handle-keypress)
 
 (defn canvas []
   [:div {:id "game-canvas"}])
@@ -45,28 +22,8 @@
    [:p "Snake game"]
    [canvas]])
 
-(defn setup []
-  (q/frame-rate 25) 
-  (q/background 0))
 
-(defn draw []
-  (q/background 0)
-  (q/stroke 0)
-  (q/stroke-weight 2)
-  (q/fill 0 255 0)
-  (doseq [[x y] @snake-body1]
-    (q/rect x y 20 20))
-  (q/fill 255 0 0)
-  (doseq [[x y] @snake-body2]
-    (q/rect x y 20 20)))
 
 (rdom/render [app] (.getElementById js/document "app"))
-
-(q/sketch
- :title "Snake game"
- :host "game-canvas"
- :settings #(q/smooth 2)
- :setup setup
- :draw draw
- :size [700 500])
-
+(connect_socket)
+(start_game)
---
+++ client/src/client/game.cljs
@@ -0,0 +1,58 @@
+(ns client.game
+  (:require 
+            [reagent.core :as r]
+            [quil.core :as q]
+            [clojure.edn :as edn]))
+
+(def snake-body1 (r/atom [[100 100]]))
+(def snake-body2 (r/atom [[300 200]]))
+
+
+(defn connect_socket []
+  (let [ws (js/WebSocket. "ws://localhost:8080/ws")]
+    (.addEventListener ws "open" (fn [_] (println "WebSocket connected")))
+    (.addEventListener ws "message" (fn [e]
+                                      (let [data (edn/read-string (.-data e))]
+                                         ;(println data)
+                                        (reset! snake-body1 (:snake1 data))
+                                        (reset! snake-body2 (:snake2 data)))))
+    (.addEventListener ws "close" (fn [_] (println "WebSocket closed")))
+    (let [handle-keypress (fn handle-keypress [e]
+                            (let [key (.-key e)]
+                              (case key
+                                "ArrowUp" (.send ws {:direction :up})
+                                "ArrowDown" (.send ws {:direction :down})
+                                "ArrowLeft" (.send ws {:direction :left})
+                                "ArrowRight" (.send ws {:direction :right}))))]
+      (.addEventListener js/document "keydown" handle-keypress)
+      ) 
+))
+
+(defn setup []
+  (q/frame-rate 25)
+  (q/background 0))
+
+(defn draw []
+  (q/background 0)
+  (q/stroke 0)
+  (q/stroke-weight 2)
+  (q/fill 0 255 0)
+  (doseq [[x y] @snake-body1]
+    (q/rect x y 20 20))
+  (q/fill 255 0 0)
+  (doseq [[x y] @snake-body2]
+    (q/rect x y 20 20)))
+
+
+(defn start_game []
+  (q/sketch
+   :title "Snake game"
+   :host "game-canvas"
+   :settings #(q/smooth 2)
+   :setup setup
+   :draw draw
+   :size [700 500])
+  )
+
+
+
