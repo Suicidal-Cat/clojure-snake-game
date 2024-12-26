003596474f0d643ded295b6521dd466c918e5b37
Suicidal-Cat <darekolaric@gmail.com>
2024-12-26 14:11:07 +0100

    refeactor server side code

--- server/src/server/core.clj
+++ server/src/server/core.clj
@@ -1,35 +1,11 @@
 (ns server.core
   (:gen-class)
-  (:require [compojure.core :refer [defroutes GET]]
-            [ring.adapter.jetty :refer [run-jetty]]
-            [ring.websocket :as ws]
-            [clojure.edn :as edn]))
-
-;hash-map - :snake array [X Y]
-(def game-state (atom {:snake1 [[100 100] [120 100] [140 100] [160 100] [180 100]]
-                       :snake2 [[300 120] [300 140] [300 160] [300 180] [300 200]]}))
-
-;hash-map - :playerid :direction
-(def snakes-direction (atom {:snake1 :right
-                            :snake2 :left}))
-;update snake by direction
-(defn move-snake [snake direction]
-  (let [[x y] (first snake)]
-    (case direction
-      :up    (cons [x (- y 20)] (butlast snake))
-      :down  (cons [x (+ y 20)] (butlast snake))
-      :left  (cons [(- x 20) y] (butlast snake))
-      :right (cons [(+ x 20) y] (butlast snake)))))
-
-;update direction
-(defn change-direction [player dir]
-  (let [past-dir (player @snakes-direction)]
-    (if (not= past-dir dir)
-      (cond
-        (and (= past-dir :up) (not= dir :down)) (swap! snakes-direction update player (fn [_] dir))
-        (and (= past-dir :down) (not= dir :up)) (swap! snakes-direction update player (fn [_] dir))
-        (and (= past-dir :left) (not= dir :right)) (swap! snakes-direction update player (fn [_] dir))
-        (and (= past-dir :right) (not= dir :left)) (swap! snakes-direction update player (fn [_] dir))))))
+  (:require
+   [clojure.edn :as edn]
+   [compojure.core :refer [defroutes GET]]
+   [ring.adapter.jetty :refer [run-jetty]]
+   [ring.websocket :as ws]
+   [server.game :refer [broadcast-game-state change-direction stop-game]]))
 
 (defn handle-message [message]
   (let [data (edn/read-string message)
@@ -38,24 +14,12 @@
       (println (:id data))
       (change-direction :snake1 dir))))
 
-(def stop-flag (atom false))
-
-(defn broadcast-game-state [socket]
-  (future
-    (reset! stop-flag false)
-    (while (not @stop-flag)
-      (Thread/sleep 90)
-      (swap! game-state update :snake1 move-snake (:snake1 @snakes-direction))
-      (ws/send socket (pr-str @game-state)))))
-
 
 (defn echo-handler [request]
   (assert (ws/upgrade-request? request))
   {::ws/listener
    {:on-open
-    (fn [socket]
-      (reset! game-state {:snake1 [[180 100] [160 100] [140 100] [120 100] [100 100]]
-                          :snake2 [[300 120] [300 140] [300 160] [300 180] [300 200]]})
+    (fn [socket] 
       (broadcast-game-state socket))
     :on-message
     (fn [socket message]
@@ -64,7 +28,7 @@
         (handle-message message)))
     :on-close
     (fn [socket code reason]
-      (reset! stop-flag true))}})
+      stop-game)}})
 
 (defroutes app-routes
   (GET "/" [] "<h1>HELLO<h1>")
---
+++ server/src/server/game.clj
@@ -0,0 +1,45 @@
+(ns server.game
+  (:require [ring.websocket :as ws]))
+
+;hash-map - :snake array [X Y]
+(def game-state (atom {:snake1 [[100 100] [120 100] [140 100] [160 100] [180 100]]
+                       :snake2 [[300 120] [300 140] [300 160] [300 180] [300 200]]}))
+
+;hash-map - :playerid :direction
+(def snakes-direction (atom {:snake1 :right
+                            :snake2 :left}))
+;update snake by direction
+(defn move-snake [snake direction]
+  (let [[x y] (first snake)]
+    (case direction
+      :up    (cons [x (- y 20)] (butlast snake))
+      :down  (cons [x (+ y 20)] (butlast snake))
+      :left  (cons [(- x 20) y] (butlast snake))
+      :right (cons [(+ x 20) y] (butlast snake)))))
+
+;update direction
+(defn change-direction [player dir]
+  (let [past-dir (player @snakes-direction)]
+    (if (not= past-dir dir)
+      (cond
+        (and (= past-dir :up) (not= dir :down)) (swap! snakes-direction update player (fn [_] dir))
+        (and (= past-dir :down) (not= dir :up)) (swap! snakes-direction update player (fn [_] dir))
+        (and (= past-dir :left) (not= dir :right)) (swap! snakes-direction update player (fn [_] dir))
+        (and (= past-dir :right) (not= dir :left)) (swap! snakes-direction update player (fn [_] dir))))))
+
+
+
+(def stop-flag (atom false))
+
+(defn broadcast-game-state [socket]
+  (future
+    (reset! stop-flag false)
+    (reset! game-state {:snake1 [[180 100] [160 100] [140 100] [120 100] [100 100]]
+                        :snake2 [[300 120] [300 140] [300 160] [300 180] [300 200]]})
+    (while (not @stop-flag)
+      (Thread/sleep 90)
+      (swap! game-state update :snake1 move-snake (:snake1 @snakes-direction))
+      (ws/send socket (pr-str @game-state)))))
+
+(defn stop-game []
+  (reset! stop-flag true))
